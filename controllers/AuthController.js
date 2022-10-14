30cefb295a143d31d19fc0dfaa3c546460bf18f7
Diogo Batista <diogo.ariau@gmail.com>
2022-10-13 20:17:17 -0400

    criando o resto do crud e melhorando a home

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,4 +1,6 @@
+const User = require('../models/User')
 
+const bcrypt = require('bcryptjs')
 module.exports = class AuthController {
     static async register(req, res) {
         res.render('auth/register')
@@ -6,15 +8,83 @@ module.exports = class AuthController {
     static async login(req, res) {
         res.render('auth/login')
     }
+    static async loginPost(req, res) {
+        //find and check user
+        const {email, password} = req.body
+
+        const user = await User.findOne({where:{email: email}})
+
+        if(!user){
+            req.flash('message', 'Usuário não encontrado')
+            res.render('auth/login')
+            return
+        }
+        //check password
+        const passwordMatch = bcrypt.compareSync(password, user.password)
+
+        if(!passwordMatch){
+            req.flash('message', 'Senha incorreta, tente novamente')
+            res.render('auth/login')
+            return
+        }
+        //initializing session
+        try{
+            req.session.userId = user.id
+            req.flash('message', 'Usuário autenticado')
+            req.session.save(()=>{
+                res.redirect('/')
+            })
+        }catch(err){
+            console.log(err)
+        }
+    }
     static async registerPost(req, res) {
         const {name, email, password, confirmPassword} = req.body
+        //passord match validation
         if(password != confirmPassword){
             req.flash('message', 'As senhas não conferem, tente novamente')
             res.render('auth/register')
 
             return
         }
+        //check if user exists
+        const checkIfUserExists = await User.findOne({where:{email: email}})
+
+        if(checkIfUserExists){
+            req.flash('message', 'Este email já existe')
+            res.render('auth/register')
+
+            return
+        }
+
+        //create a password
+        const salt = bcrypt.genSaltSync(10)
+        const hashedPassword = bcrypt.hashSync(password, salt)
+
+        const user = {
+            name,
+            email,
+            password: hashedPassword
+        }
+
+        try{
+            const createdUser = await User.create(user)
+
+            req.session.userId = createdUser.id
+
+            req.flash('message', 'Cadastro realizado com sucesso')
+
+            req.session.save(()=>{
+                res.redirect('/');
+            })
+        }catch(err){
+            console.log(err)
+        }
     }
+    static logout(req, res) {
+        req.session.destroy()
+        res.redirect('/login')
+      }
     
 }
 
--- controllers/ThoughtController.js
+++ controllers/ThoughtController.js
@@ -3,6 +3,93 @@ const User = require('../models/User')
 
 module.exports = class ThoughtController {
     static async showThoughts(req, res) {
-        res.render('thoughts/home')
+
+        const thoughtsData = await Thought.findAll({include:User})
+
+        const thoughts = thoughtsData.map((result) => result.get({plain:true}))
+
+        console.log(thoughts)
+        res.render('thoughts/home', {thoughts})
     }
+    static async dashboard(req, res) {
+        const userId = req.session.userId
+
+        const user = await User.findOne({
+            where:{id: userId},
+            include:Thought,
+            plain:true,
+        })
+
+        if(!user){
+            res.redirect('/login')
+        }
+        const thoughts = user.Thoughts.map((result) => result.dataValues)
+        console.log(thoughts)
+
+        let emptyThoughts = false
+        if(thoughts.length === 0){
+          emptyThoughts = true
+        }
+
+        res.render('thoughts/dashboard', {thoughts, emptyThoughts})
+    }
+    
+    static createThought(req, res) {
+        res.render('thoughts/create')
+    }
+    static createThoughtSave(req, res) {
+        const thought = {
+          title: req.body.title,
+          UserId: req.session.userId,
+        }
+    
+        Thought.create(thought)
+          .then(() => {
+            req.flash('message', 'Pensamento criado com sucesso!')
+            req.session.save(() => {
+              res.redirect('/thoughts/dashboard')
+            })
+          })
+          .catch((err) => console.log())
+      }
+      static removeThought(req, res) {
+        const id = req.body.id
+    
+        Thought.destroy({ where: { id: id } })
+          .then(() => {
+            req.flash('message', 'Pensamento removido com sucesso!')
+            req.session.save(() => {
+              res.redirect('/thoughts/dashboard')
+            })
+          })
+          .catch((err) => console.log(err))
+      }
+
+      static async updateThought(req, res) {
+        const id = req.params.id
+
+        const thought = await Thought.findOne({raw:true, where:{id: id}}).then((thought)=>{
+          res.render('thoughts/edit', {thought})
+        }).catch(err => console.log(err))
+      }
+
+      static async updateThoughtSave(req, res){
+        const id = req.body.id
+
+        const thought = {
+          title:req.body.title
+        }
+
+        try{
+          await Thought.update(thought, {where:{id: id}})
+
+          req.flash('message', 'Você editou um pensamento')
+
+          req.session.save(()=>{
+            res.redirect('/thoughts/dashboard')
+          })
+        }catch(err){
+          console.log(err)
+        }
+      }
 }
